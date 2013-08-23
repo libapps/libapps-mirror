5f03d9718c153b17b7000f3dcca535a1ccd1cbd2
Robert Ginda <rginda@chromium.org>
2013-08-23 13:29:26 -0700

    update crosh_buitin zip file to latest crosh+hterm
    BUG=chromium:245700
    TEST=manual testing
    Change-Id: I2103777fece2d5dd46c1daf7c3139c91c1ea3030
    Reviewed-on: https://chromium-review.googlesource.com/66856
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Robert Ginda <rginda@chromium.org>
    Tested-by: Robert Ginda <rginda@chromium.org>

---
+++ crosh_builtin/rezip.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Pass this an existing Secure Shell zip from (made from ../nassh/bin/mkzip.sh)
+# and it'll turn it into a crosh_builtin.zip file.
+
+if [ -z "$1" ]; then
+  echo "Missing argument."
+  exit 1
+fi
+
+SSZIP="$(readlink -f "$1")"
+if [ ! -e "$SSZIP" ]; then
+  echo "Not found: $SSZIP"
+  exit 1
+fi
+
+cd "$(readlink -f "$(dirname "$0")")"
+
+# Fail on error.
+set -e
+
+# Remove previous work
+rm -f crosh_builtin.zip
+rm -rf tmp
+
+# Unzip the given Secure Shell zip
+mkdir -p tmp
+(cd tmp; unzip -q "$SSZIP")
+
+# Save the manifest file.  You'll need to convert this to a crosh manifest
+# file and check it into src/chrome/browser/resources/chromeos/crosh_builtin/
+# manifest.json
+mv tmp/manifest.json .
+
+# Remove things we don't need for crosh.
+rm -rf tmp/html/nassh.html tmp/plugin/
+
+# Fix permissions.
+chmod -R a+r tmp/
+chmod a+x $(find tmp/ -type d)
+
+# Remake the zip.
+(cd tmp; zip -qr ../crosh_builtin.zip .)
+
+# Echo the zip file listing to the terminal for verification.
+unzip -l crosh_builtin.zip
+
+echo
+echo "HEY!"
+echo "You'll need to convert ./manifest.json to crosh and commit it to"
+echo "src/chrome/browser/resources/chromeos/crosh_builtin/manifest.json"
