31b63e13b6a5b8b625f1cccffd482e0212cd7234
Robert Ginda <rginda@chromium.org>
2013-11-25 16:53:25 -0800

    update links
    Change-Id: I33aed5426ed3bc85727678b0da64b20ba006fdd5
    Reviewed-on: https://chromium-review.googlesource.com/177960
    Reviewed-by: Robert Ginda <rginda@chromium.org>
    Tested-by: Robert Ginda <rginda@chromium.org>

--- nassh/js/nassh_command_instance.js
+++ nassh/js/nassh_command_instance.js
@@ -119,7 +119,7 @@ nassh.CommandInstance.prototype.run = function() {
     }
 
     this.io.println(
-        nassh.msg('WELCOME_FAQ', ['\x1b[1mhttp://goo.gl/m6Nj8\x1b[m']));
+        nassh.msg('WELCOME_FAQ', ['\x1b[1mhttp://goo.gl/TK7876\x1b[m']));
 
     if (hterm.windowType != 'popup') {
       var osx = window.navigator.userAgent.match(/Mac OS X/);
@@ -127,7 +127,7 @@ nassh.CommandInstance.prototype.run = function() {
         this.io.println('');
         this.io.println(
             nassh.msg('OPEN_AS_WINDOW_TIP',
-                      ['\x1b[1mhttp://goo.gl/OeH3i\x1b[m']));
+                      ['\x1b[1mhttp://goo.gl/TK7876\x1b[m']));
         this.io.println('');
       }
     }
