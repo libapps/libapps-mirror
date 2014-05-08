f080aa4ba42283b986950a3b2fb660f97ee21229
Robert Ginda <rginda@chromium.org>
2014-05-08 14:44:13 -0700

    saltpig: Initial commit of nacl collection v2 app
    This is an experimental app that hosts a few binaries from naclports,
    exposing their functionality over wam.FileSystem.  It's currently
    hardcoded to only speak to the "Wash (tot)" and "Wash (dev)"
    extensions.
    
    You'll need to run the ./bin/get_nacl.sh file to fetch the necessary
    nacl binaries from the web, and ./bin/mkdeps.sh to build the js
    dependencies.
    
    Change-Id: I5db5003903854bdc45c7c6b81eb482d2a47e7a26
    Reviewed-on: https://chromium-review.googlesource.com/198908
    Reviewed-by: Robert Ginda <rginda@chromium.org>
    Tested-by: Robert Ginda <rginda@chromium.org>

---
+++ saltpig/bin/mkzip.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+cd "$(readlink -f "$(dirname "$0")/..")"
+
+if [ -z "$LIBDOT_SEARCH_PATH" ]; then
+  export LIBDOT_SEARCH_PATH="$(readlink -f "../")"
+fi
+
+source "../libdot/bin/common.sh"
+
+./bin/mkdeps.sh
+
+mkdir -p ./dist/zip
+
+export MORE_FILE_PATTERNS='\./.*\.nmf \./.*\.pexe \./.*\.tar'
+
+../libdot/bin/mkzip.sh -s "." -w ./dist/zip/ "$@"
