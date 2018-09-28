d7f548fc28a2a1a13c294ca955c1637e82dce6fe
Mike Frysinger <vapier@chromium.org>
2018-09-28 15:02:27 -0400

    nassh: rework our datetime stamp in the generated version
    While Chrome is OK with loading local extensions with versions like
    "0.9.20180928.145140", the CWS rejects it.  It enforces the manifest
    version policy where each dotted field is in the range [0..65535].
    
    Rework the generated datetime stamp to pack the date/time components
    into the 16bits each one is limited to.  If we had 32bits to work
    with, we could have used bit shifts to be a bit cleaner.
    
    Change-Id: I3eab0153535a1c61c9802d086345e9d5a0fd79c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1252402
    Reviewed-by: Vitaliy Shipitsyn <vsh@google.com>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- nassh/bin/mkzip.sh
+++ nassh/bin/mkzip.sh
@@ -17,7 +17,37 @@ export MORE_FILE_PATTERNS_EXCLUDE='
   .*/images/\(promo\|screenshot\)-.*\.\(jpg\|png\)$
 '
 
-stamp="$(date +%Y%m%d.%H%M%S)"
+# The CWS limits each dotted field to 16bits (65535), and allows only 4 fields.
+# That means we have to pack the year/month/day and hour/minutes/seconds.
+#   https://developer.chrome.com/extensions/manifest/version
+# If we're creative, we can pack these so the version is always increasing.
+#
+# We're really just worried about two consecutive builds not decreasing.
+# Keep in mind that we hand maintain the first two components of the version in
+# the manifest.json.
+
+# The _ prefix is to use spaces instead of leading zeros.
+#                $1  $2  $3  $4  $5  $6
+set -- $(date +'%_y %_m %_d %_H %_M %_S')
+
+# The first field is the date.
+# %y: last two digits of the year [0..99]
+# %m: month [1..12]
+# %d: day [1..31]
+# Field = (year + 12 + 31) + (month + 31) + day
+# This is OK because 99 + 12 + 31 = 142 < 255.
+stamp_date=$(( ($1 + 12 + 31) + ($2 + 31) + $3 ))
+
+# The second field is the time.
+# %H: hour [0..23]
+# %M: minute [0..59]
+# %S: seconds [0..60] -- includes leap second
+# Field = (hour + 59 + 60) + (minute + 59) + second
+# This is OK because 23 + 59 + 60 = 142 < 255.
+stamp_time=$(( ($4 + 59 + 60) + ($5 + 59) + $6 ))
+
+stamp="${stamp_date}.${stamp_time}"
+
 for manifest in manifest_*.json; do
   mkzip.sh --append_version "${stamp}" \
     -s "." -w ./dist/zip/ -m "${manifest}" "$@"
