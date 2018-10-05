d76a00bd426dbbfce11ae19258b35bb5fc55b905
Mike Frysinger <vapier@chromium.org>
2018-10-05 10:43:41 -0400

    nassh: mkzip: fix $@ clobbering
    The timestamp logic was reusing the $@ array which clobbered the args
    from the user.  Put this into a dedicated function so we get our own
    copy of $@ to play with.
    
    Change-Id: Iade68e283268983c77c978e5a19a378b12b291a8
    Reviewed-on: https://chromium-review.googlesource.com/c/1264066
    Reviewed-by: Vitaliy Shipitsyn <vsh@google.com>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- nassh/bin/mkzip.sh
+++ nassh/bin/mkzip.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -25,28 +25,30 @@ export MORE_FILE_PATTERNS_EXCLUDE='
 # We're really just worried about two consecutive builds not decreasing.
 # Keep in mind that we hand maintain the first two components of the version in
 # the manifest.json.
+set_stamp() {
+  # The _ prefix is to use spaces instead of leading zeros.
+  #                $1  $2  $3  $4  $5  $6
+  set -- $(date +'%_y %_m %_d %_H %_M %_S')
 
-# The _ prefix is to use spaces instead of leading zeros.
-#                $1  $2  $3  $4  $5  $6
-set -- $(date +'%_y %_m %_d %_H %_M %_S')
+  # The first field is the date.
+  # %y: last two digits of the year [0..99]
+  # %m: month [1..12]
+  # %d: day [1..31]
+  # Field = (year + 12 + 31) + (month + 31) + day
+  # This is OK because 99 + 12 + 31 = 142 < 255.
+  local stamp_date=$(( ($1 + 12 + 31) + ($2 + 31) + $3 ))
 
-# The first field is the date.
-# %y: last two digits of the year [0..99]
-# %m: month [1..12]
-# %d: day [1..31]
-# Field = (year + 12 + 31) + (month + 31) + day
-# This is OK because 99 + 12 + 31 = 142 < 255.
-stamp_date=$(( ($1 + 12 + 31) + ($2 + 31) + $3 ))
+  # The second field is the time.
+  # %H: hour [0..23]
+  # %M: minute [0..59]
+  # %S: seconds [0..60] -- includes leap second
+  # Field = (hour + 59 + 60) + (minute + 59) + second
+  # This is OK because 23 + 59 + 60 = 142 < 255.
+  local stamp_time=$(( ($4 + 59 + 60) + ($5 + 59) + $6 ))
 
-# The second field is the time.
-# %H: hour [0..23]
-# %M: minute [0..59]
-# %S: seconds [0..60] -- includes leap second
-# Field = (hour + 59 + 60) + (minute + 59) + second
-# This is OK because 23 + 59 + 60 = 142 < 255.
-stamp_time=$(( ($4 + 59 + 60) + ($5 + 59) + $6 ))
-
-stamp="${stamp_date}.${stamp_time}"
+  stamp="${stamp_date}.${stamp_time}"
+}
+set_stamp
 
 for manifest in manifest_*.json; do
   mkzip.sh --append_version "${stamp}" \
