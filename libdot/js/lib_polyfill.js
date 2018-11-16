886250912df3a6dcb87c2b41b97a9a168c9f81ea
Mike Frysinger <vapier@chromium.org>
2018-11-16 02:28:34 -0500

    libdot: polyfill: add Promise.finally
    All current browsers support this, but it's new to ES2018.
    
    Change-Id: I727b1a115a8c9e3528d6cf993fa59b979b483d0e
    Reviewed-on: https://chromium-review.googlesource.com/c/1343303
    Reviewed-by: Vitaliy Shipitsyn <vsh@google.com>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- libdot/js/lib_polyfill.js
+++ libdot/js/lib_polyfill.js
@@ -68,3 +68,51 @@ if (!Object.values || !Object.entries) {
     };
   }
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally
+// https://github.com/tc39/proposal-promise-finally/blob/master/polyfill.js
+if (typeof Promise.prototype.finally !== 'function') {
+  const speciesConstructor = function(O, defaultConstructor) {
+    if (!O || (typeof O !== 'object' && typeof O !== 'function')) {
+      throw new TypeError('Assertion failed: Type(O) is not Object');
+    }
+    const C = O.constructor;
+    if (typeof C === 'undefined') {
+      return defaultConstructor;
+    }
+    if (!C || (typeof C !== 'object' && typeof C !== 'function')) {
+      throw new TypeError('O.constructor is not an Object');
+    }
+    const S =
+        typeof Symbol === 'function' && typeof Symbol.species === 'symbol' ?
+        C[Symbol.species] : undefined;
+    if (S == null) {
+      return defaultConstructor;
+    }
+    if (typeof S === 'function' && S.prototype) {
+      return S;
+    }
+    throw new TypeError('no constructor found');
+  };
+
+  const shim = {
+    finally(onFinally) {
+      const promise = this;
+      if (typeof promise !== 'object' || promise === null) {
+        throw new TypeError('"this" value is not an Object');
+      }
+      const C = speciesConstructor(promise, Promise);
+      if (typeof onFinally !== 'function') {
+        return Promise.prototype.then.call(promise, onFinally, onFinally);
+      }
+      return Promise.prototype.then.call(
+        promise,
+        x => new C(resolve => resolve(onFinally())).then(() => x),
+        e => new C(resolve => resolve(onFinally())).then(() => { throw e; })
+      );
+    }
+  };
+  Object.defineProperty(Promise.prototype, 'finally', {
+    configurable: true, writable: true, value: shim.finally,
+  });
+}
