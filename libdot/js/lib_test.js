638ad5ac598fe5fe022bb1499100bec941183d2e
Mike Frysinger <vapier@chromium.org>
2019-04-08 10:39:27 +1000

    libdot: test: rework to catch early errors
    Unify the test framework setup so it's in one file and comes before
    anything else.  This allows us to catch random errors (syntax, etc...)
    that might show up in the files we load before starting the tests.
    
    Change-Id: Iae36243c6e58c6a87aa3f89c7be2399cd1d9fc1d
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/1557633
    Reviewed-by: Vitaliy Shipitsyn <vsh@google.com>
    Reviewed-by: Damián Furman <damianfurman@google.com>
    Tested-by: Damián Furman <damianfurman@google.com>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- libdot/js/lib_test.js
+++ libdot/js/lib_test.js
@@ -4,12 +4,30 @@
 
 'use strict';
 
+/**
+ * @fileoverview Test framework setup when run inside the browser.
+ */
+
+// Setup the mocha framework.
+mocha.setup('bdd');
+mocha.checkLeaks();
+
 // Add a global shortcut to the assert API.
 const assert = chai.assert;
 
+// Catch any random errors before the test runner runs.
+let earlyError = null;
+window.onerror = function() {
+  earlyError = Array.from(arguments);
+};
+
+// Run the test framework once everything is finished.
 window.onload = function() {
   lib.init(() => {
-    mocha.checkLeaks();
     mocha.run();
+
+    if (earlyError !== null) {
+      assert.fail(`uncaught exception detected:\n${earlyError.join('\n')}\n`);
+    }
   });
 };
