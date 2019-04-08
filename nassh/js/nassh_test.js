aae33c013639a65bcd6dd18dc8e32ca4fee9e449
Mike Frysinger <vapier@chromium.org>
2019-04-08 10:40:41 +1000

    nassh: test: rework to catch early errors
    Unify the test framework setup so it's in one file and comes before
    anything else.  This allows us to catch random errors (syntax, etc...)
    that might show up in the files we load before starting the tests.
    
    Change-Id: I64bea47a3916de5b9544234e01be1d98d759ce8d
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/1557634
    Reviewed-by: Vitaliy Shipitsyn <vsh@google.com>
    Reviewed-by: Damián Furman <damianfurman@google.com>
    Tested-by: Damián Furman <damianfurman@google.com>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- nassh/js/nassh_test.js
+++ nassh/js/nassh_test.js
@@ -4,15 +4,33 @@
 
 'use strict';
 
+/**
+ * @fileoverview Test framework setup when run inside the browser.
+ */
+
+// Setup the mocha framework.
+mocha.setup('bdd');
+mocha.checkLeaks();
+
 // Add a global shortcut to the assert API.
 const assert = chai.assert;
 
+// Catch any random errors before the test runner runs.
+let earlyError = null;
+window.onerror = function() {
+  earlyError = Array.from(arguments);
+};
+
+// Run the test framework once everything is finished.
 window.onload = function() {
   hterm.defaultStorage = new lib.Storage.Memory();
   nassh.defaultStorage = new lib.Storage.Memory();
 
   lib.init(() => {
-    mocha.checkLeaks();
     mocha.run();
+
+    if (earlyError !== null) {
+      assert.fail(`uncaught exception detected:\n${earlyError.join('\n')}\n`);
+    }
   });
 };
