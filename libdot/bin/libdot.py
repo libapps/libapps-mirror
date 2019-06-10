668a15b3209e62eb189a6fe80c87177763b9420c
Mike Frysinger <vapier@chromium.org>
2019-06-10 16:05:26 -0400

    libdot/hterm/nassh: convert load_tests.sh to python
    This allows us to standardize the interface across all of them and
    kill off one more source of shell scripts in the tree.
    
    For hterm/nassh, we have to create little python modules to help
    bootstrap their logic (since this is the first python either has
    seen).  The style is cribbed from the existing ssh_client.py.
    
    Change-Id: I2478f598c574cbff8ca1168e3a366b7a5031a964
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/1652244
    Reviewed-by: Vitaliy Shipitsyn <vsh@google.com>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- libdot/bin/libdot.py
+++ libdot/bin/libdot.py
@@ -8,9 +8,11 @@
 
 from __future__ import print_function
 
+import argparse
 import logging
 import logging.handlers
 import os
+import subprocess
 import sys
 import time
 
@@ -40,3 +42,51 @@ def setup_logging(debug=False):
     logger = logging.getLogger()
     logger.addHandler(handler)
     logger.setLevel(level)
+
+
+def html_test_runner_parser():
+    """Get a parser for our test runner."""
+    parser = argparse.ArgumentParser(
+        description='HTML test runner',
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Run with debug output.')
+    parser.add_argument('--browser', default=os.getenv('CHROME_BIN'),
+                        help='Browser program to run tests against.')
+    parser.add_argument('--profile', default=os.getenv('CHROME_TEST_PROFILE'),
+                        help='Browser profile dir to run against.')
+    return parser
+
+
+def html_test_runner_main(argv, path):
+    """Open the test page at |path|."""
+    parser = html_test_runner_parser()
+    opts = parser.parse_args(argv)
+    setup_logging(debug=opts.debug)
+
+    # Try to use default X session.
+    os.environ.setdefault('DISPLAY', '0:0')
+
+    # Set up a unique profile to avoid colliding with user settings.
+    profile_dir = opts.profile
+    if not profile_dir:
+        profile_dir = os.path.expanduser('~/.config/google-chrome-run_local')
+    os.makedirs(profile_dir, exist_ok=True)
+
+    # Chrome goes by many names.  We know them all!
+    browser = opts.browser
+    if not browser:
+        for suffix in ('', '-stable', '-beta', '-unstable', '-trunk'):
+            browser = 'google-chrome%s' % (suffix,)
+            try:
+                subprocess.check_call([browser, '--version'])
+                break
+            except (FileNotFoundError, subprocess.CalledProcessError):
+                pass
+        else:
+            parser.error('Could not find a browser; please use --browser.')
+
+    # Kick off test runner in the background so we exit.
+    logging.info('Running tests against browser "%s".', browser)
+    logging.info('Tests page: %s', path)
+    subprocess.Popen([browser, '--user-data-dir=%s' % (profile_dir,), path])
