e833f4bdf4e409ddd696ed708d99b17d89cf7eda
Julian Watson <juwa@google.com>
2019-10-29 13:15:34 +1100

    terminal: add saturation and lightness picker
    Change-Id: Ieeb4bbbcbba64ed988b6492c13ff96dda57f414e
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/1886091
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>

---
+++ terminal/js/saturation_lightness_picker.js
@@ -0,0 +1,133 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Exports an element: saturation-lightness-picker
+ *
+ * @suppress {moduleLoad}
+ */
+import {LitElement, css, html} from './lit_element.js';
+
+export class SaturationLightnessPickerElement extends LitElement {
+  static get is() { return 'saturation-lightness-picker'; }
+
+  /** @override */
+  static get properties() {
+    return {
+      hue: {
+        type: Number,
+      },
+      saturation: {
+        type: Number,
+        reflect: true,
+      },
+      lightness: {
+        type: Number,
+        reflect: true,
+      },
+    };
+  }
+
+  static get styles() {
+    return css`
+        :host {
+          box-shadow: 1px 1px 2px rgba(0,0,0,0.3);
+          cursor: pointer;
+          display: block;
+          height: 160px;
+          position: relative;
+          width: 200px;
+        }
+
+        :host::before {
+          background-image: linear-gradient(
+              to right,
+              hsl(0, 0%, 50%),
+              transparent);
+          bottom: 0;
+          content: "";
+          left: 0;
+          pointer-events: none;
+          position: absolute;
+          right: 0;
+          top: 0;
+          z-index: 1;
+        }
+
+        :host::after {
+          background-image: linear-gradient(
+              to top,
+              black,
+              transparent,
+              white);
+          bottom: 0;
+          content: "";
+          left: 0;
+          pointer-events: none;
+          position: absolute;
+          right: 0;
+          top: 0;
+          z-index: 1;
+        }
+
+        #display {
+          border-radius: inherit;
+          height: 100%;
+          pointer-events: none;
+          width: 100%;
+        }
+
+        #picker {
+          border-radius: 100%;
+          border: 2px solid white;
+          box-shadow: 1px 1px 2px;
+          box-sizing: border-box;
+          cursor: pointer;
+          height: 24px;
+          left: 50%;
+          pointer-events: none;
+          position: absolute;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          width: 24px;
+          z-index: 2;
+        }
+    `;
+  }
+
+  /** @override */
+  render() {
+    return html`
+        <div id="display" style="background-color: hsl(${this.hue}, 100%, 50%);">
+        </div>
+        <div id="picker" style="left: ${this.saturation}%; top: ${100 - this.lightness}%; background-color: hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%);"></div>
+        </div>
+    `;
+  }
+
+  /** @override */
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.addEventListener('click', this.onClick_);
+  }
+
+  /** @override */
+  disconnectedCallback() {
+    this.removeEventListener('click', this.onClick_);
+
+    super.disconnectedCallback();
+  }
+
+  /** @param {!Event} event */
+  onClick_(event) {
+    const xPercent = event.offsetX / this.clientWidth;
+    const yPercent = event.offsetY / this.clientHeight;
+
+    this.saturation = 100 * xPercent;
+    this.lightness = 100 * (1 - yPercent);
+
+    this.dispatchEvent(new CustomEvent('updated', {bubbles: true}));
+  }
+}
---
+++ terminal/js/saturation_lightness_picker_tests.js
@@ -0,0 +1,118 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Sauration Lightness Picker unit tests
+ */
+
+import {SaturationLightnessPickerElement as Element} from './saturation_lightness_picker.js';
+
+describe('saturation_lightness_picker_tests.js', () => {
+  const createElement = (saturation, lightness) => {
+    const el = document.createElement(Element.is);
+    el.setAttribute('saturation', saturation);
+    el.setAttribute('lightness', lightness);
+    return el;
+  };
+
+  const getPicker = el => el.shadowRoot.getElementById('picker');
+
+  before(function() {
+    if (customElements.get(Element.is) === undefined) {
+      customElements.define(Element.is, Element);
+    }
+  });
+
+  afterEach(function() {
+    document.querySelectorAll(Element.is)
+        .forEach(el => el.parentElement.removeChild(el));
+  });
+
+  it('initialises-the-picker-to-the-correct-coordinates', async function() {
+    const els = [
+      createElement(0, 0),
+      createElement(0, 50),
+      createElement(0, 100),
+      createElement(50, 0),
+      createElement(50, 50),
+      createElement(50, 100),
+      createElement(100, 0),
+      createElement(100, 50),
+      createElement(100, 100)
+    ];
+
+    els.forEach(el => document.body.appendChild(el));
+    await Promise.all(els.map(el => el.updateComplete));
+
+    assert.deepEqual(
+        els.map(getPicker).map(el => [el.style.left, el.style.top]), [
+          ['0%', '100%'],
+          ['0%', '50%'],
+          ['0%', '0%'],
+          ['50%', '100%'],
+          ['50%', '50%'],
+          ['50%', '0%'],
+          ['100%', '100%'],
+          ['100%', '50%'],
+          ['100%', '0%']
+        ]);
+  });
+
+  it('updates-picker-location-when-attribute-changed', async function() {
+    const el = createElement(25, 25);
+
+    document.body.appendChild(el);
+    await el.updateComplete;
+
+    assert.equal(getPicker(el).style.left, '25%');
+    assert.equal(getPicker(el).style.top, '75%');
+
+    el.setAttribute('saturation', 75);
+    await el.updateComplete;
+
+    assert.equal(getPicker(el).style.left, '75%');
+    assert.equal(getPicker(el).style.top, '75%');
+
+    el.setAttribute('lightness', 75);
+    await el.updateComplete;
+
+    assert.equal(getPicker(el).style.left, '75%');
+    assert.equal(getPicker(el).style.top, '25%');
+  });
+
+  it('updates-picker-location-when-clicked', async function() {
+    const el = createElement(25, 25);
+
+    document.body.appendChild(el);
+    await el.updateComplete;
+
+    assert.equal(getPicker(el).style.left, '25%');
+    assert.equal(getPicker(el).style.top, '75%');
+
+    // Manually call handler, as you can't set offsetX/offsetY on a custom mouse
+    // event.
+    el.onClick_(
+        {offsetX: el.clientWidth * 0.75, offsetY: el.clientHeight * 0.25});
+    await el.updateComplete;
+
+    assert.equal(getPicker(el).style.left, '75%');
+    assert.equal(getPicker(el).style.top, '25%');
+    assert.equal(el.getAttribute('saturation'), 75);
+    assert.equal(el.getAttribute('lightness'), 75);
+  });
+
+  it('publishes-event-when-clicked', async function() {
+    const el = createElement(25, 25);
+
+    document.body.appendChild(el);
+    await el.updateComplete;
+
+    let listenerInvocations = 0;
+    el.addEventListener('updated', () => ++listenerInvocations);
+    el.dispatchEvent(new MouseEvent('click'));
+    await el.updateComplete;
+
+    assert.equal(listenerInvocations, 1);
+  });
+});
