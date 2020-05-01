96ebc3b9ab9bc4888375192a47838705589ba1ee
Joel Hockey <joelhockey@chromium.org>
2020-05-01 16:38:19 -0700

    terminal: use chrome.tabs.create for openOptionsPage
    Ensures that settings opens in a new tab in the same APP browser rather
    than in a new NORMAL browser.
    
    Change-Id: If190d879eb908709074ac10834e84100104a4431
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/2172937
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- terminal/js/terminal.js
+++ terminal/js/terminal.js
@@ -50,7 +50,11 @@ terminal.msg = function(name, args) {
  * Create a new window to the options page for customizing preferences.
  */
 terminal.openOptionsPage = function() {
-  lib.f.openWindow('/html/terminal_settings.html');
+  // Use chrome.tabs.create directly rather than lib.f.openWindow to ensure it
+  // opens in the current terminal window. lib.f.openWindow always opens an
+  // empty window first which opens in a NORMAL browser.
+  chrome.tabs.create(
+      {url: 'chrome-untrusted://terminal/html/terminal_settings.html'});
 };
 
 /**
