db77cae3925ab64d2b0c60e4edf1c66f100c020c
Mike Frysinger <vapier@chromium.org>
2020-05-10 14:14:51 -0400

    libdot: eslint: split out into dedicated tool
    Make it easy to run just eslint by splitting it out into a dedicated
    wrapper script.  This will make it easier to convert the generic lint
    tool to lint every possible source file.
    
    Change-Id: Id7ea2210c266698b1f6d012719990ea345fe5474
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/2195744
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- libdot/bin/libdot.py
+++ libdot/bin/libdot.py
@@ -378,6 +378,7 @@ class HelperProgram:
 # Wrappers around libdot/bin/ programs for other tools to access directly.
 closure_compiler = HelperProgram('closure-compiler')
 concat = HelperProgram('concat')
+eslint = HelperProgram('eslint')
 headless_chrome = HelperProgram('headless-chrome')
 lint = HelperProgram('lint')
 load_tests = HelperProgram('load_tests')
