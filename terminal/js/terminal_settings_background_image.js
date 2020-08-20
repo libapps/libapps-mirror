eab52d27824f9a1c8e17ba1b29a6ad1424e18806
Jason Lin <lxj@google.com>
2020-08-20 15:40:52 +1000

    terminal: remove background image url support
    Bug: 1026129
    Change-Id: I82dcd2a822346e983c1fb439c05c9a2eb0d0f89d
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/2366436
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>

--- terminal/js/terminal_settings_background_image.js
+++ terminal/js/terminal_settings_background_image.js
@@ -3,31 +3,12 @@
 // found in the LICENSE file.
 
 /**
- * @fileoverview Exports an element: terminal-settings-background.
+ * @fileoverview Exports an element: terminal-settings-background-image.
  *
  * @suppress {moduleLoad}
  */
-import {css, html, unsafeCSS} from './lit_element.js';
-import {TerminalSettingsElement} from './terminal_settings_element.js';
-import {stylesDialog} from './terminal_settings_styles.js';
+import {css, html, LitElement, unsafeCSS} from './lit_element.js';
 import './terminal_settings_button.js';
-import './terminal_settings_textfield.js';
-
-export const BACKGROUND_IMAGE_CONVERTER = {
-  preferenceToDisplay: (preference) => {
-    preference = preference ? preference.toString().trim() : '';
-    const result = preference.match(/^url\(['"]?(.*?)['"]?\)$/i);
-    return result ? result[1] : preference;
-  },
-  displayToPreference: (display) => {
-    display = display.trim();
-    if (!display) {
-      return '';
-    }
-    const prefix = RegExp('^https?://', 'i').test(display) ? '' : 'http://';
-    return `url(${prefix}${display})`;
-  },
-};
 
 const FOLDER_OPEN =
     '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">' +
@@ -35,17 +16,18 @@ const FOLDER_OPEN =
     '0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z" fill="rgb(26, 115, 232)"/>' +
     '</svg>';
 
-export class TerminalSettingsBackgroundImageElement extends
-    TerminalSettingsElement {
+const PREFERENCE = 'background-image';
+
+export class TerminalSettingsBackgroundImageElement extends LitElement {
   static get is() { return 'terminal-settings-background-image'; }
 
   /** @override */
   static get properties() {
     return {
-      imagePreviewSrc: {
+      imagePreviewSrc_: {
         type: String,
       },
-      errorMsg: {
+      errorMsg_: {
         type: String,
       },
     };
@@ -53,7 +35,7 @@ export class TerminalSettingsBackgroundImageElement extends
 
   /** @override */
   static get styles() {
-    return [stylesDialog, css`
+    return css`
       :host {
         align-items: center;
         display: flex;
@@ -77,12 +59,6 @@ export class TerminalSettingsBackgroundImageElement extends
         display: none;
       }
 
-      .dialog-row {
-        align-items: center;
-        display: flex;
-        flex-wrap: nowrap;
-      }
-
       .open-folder {
         background: url('data:image/svg+xml;utf8,${unsafeCSS(FOLDER_OPEN)}')
             no-repeat left;
@@ -93,115 +69,52 @@ export class TerminalSettingsBackgroundImageElement extends
       .button-left-margin {
         margin-left: 8px;
       }
-    `];
+    `;
   }
 
   constructor() {
     super();
-    this.preference = 'background-image';
     /** @private {string} */
-    this.imagePreviewSrc = '';
+    this.imagePreviewSrc_ = window.localStorage.getItem(PREFERENCE) || '';
     /** @private {string} */
-    this.errorMsg = '';
-    this.cancelValue_ = '';
+    this.errorMsg_ = '';
   }
 
   /** @override */
   render() {
     const msg = hterm.messageManager.get.bind(hterm.messageManager);
 
+    const select = html`
+      <terminal-settings-button id='bg-select' class="button-left-margin"
+        @click="${this.onOpenFile_}">
+        <span class="open-folder">${msg('SELECT_LABEL')}</span>
+      </terminal-settings-button>
+    `;
     const previewRemove = html`
-      <img src="${this.imagePreviewSrc}" @click="${this.openDialog}">
+      <img src="${this.imagePreviewSrc_}" @click="${this.onOpenFile_}">
       <terminal-settings-button id="bg-remove" class="button-left-margin"
          @click="${this.onRemove_}">
         ${msg('REMOVE_LABEL')}
       </terminal-settings-button>
     `;
-    const select = html`
-      <terminal-settings-button id="bg-select" @click="${this.onSelect_}">
-        ${msg('SELECT_LABEL')}
-      </terminal-settings-button>
-    `;
-
     return html`
-      <div class="error">${this.errorMsg}</div>
-      ${this.imagePreviewSrc ? previewRemove : select}
+      <div class="error">${this.errorMsg_}</div>
+      ${this.imagePreviewSrc_ ? previewRemove : select}
       <input id="upload" type="file" @change="${this.onFileChange_}"/>
-      <dialog>
-        <div class="dialog-title">
-          ${msg('TERMINAL_SETTINGS_BACKGROUND_IMAGE_DIALOG_TITLE')}
-        </div>
-        <div class="dialog-row">
-          <terminal-settings-textfield preference="background-image"
-              .placeholder="${msg('URL_LABEL')}"
-              .converter=${BACKGROUND_IMAGE_CONVERTER}>
-          </terminal-settings-textfield>
-          <terminal-settings-button class="button-left-margin"
-              @click="${this.onOpenFile_}">
-            <span class="open-folder">${msg('SELECT_LABEL')}</span>
-          </terminal-settings-button>
-        </div>
-        <div class="dialog-button-container">
-          <terminal-settings-button class="cancel" @click="${this.onCancel_}">
-            ${msg('CANCEL_BUTTON_LABEL')}
-          </terminal-settings-button>
-          <terminal-settings-button class="action" @click="${this.onOk_}">
-            ${msg('CONFIRM_BUTTON_LABEL')}
-          </terminal-settings-button>
-        </div>
-      </dialog>
     `;
-  }
-
-  /** @override */
-  preferenceChanged_(prefValue) {
-    super.preferenceChanged_(prefValue);
-    // Update preview img only if we don't have a local image set.
-    this.imagePreviewSrc = window.localStorage.getItem(this.preference) ||
-        BACKGROUND_IMAGE_CONVERTER.preferenceToDisplay(prefValue);
-  }
-
-  openDialog() {
-    this.cancelValue_ = this.value;
-    this.shadowRoot.querySelector('dialog').showModal();
-  }
-
-  closeDialog() {
-    this.shadowRoot.querySelector('dialog').close();
-  }
-
-  /** @param {!Event} event */
-  onSelect_(event) {
-    this.errorMsg = '';
-    this.openDialog();
   }
 
   /** @param {!Event} event */
   onOpenFile_(event) {
-    this.errorMsg = '';
+    this.errorMsg_ = '';
     this.shadowRoot.querySelector('#upload').click();
-  }
-
-  onOk_(event) {
-    this.closeDialog();
-    const url = BACKGROUND_IMAGE_CONVERTER.preferenceToDisplay(this.value);
-    if (url) {
-      window.localStorage.removeItem(this.preference);
-      this.imagePreviewSrc = url;
-    }
-  }
-
-  onCancel_(event) {
-    this.closeDialog();
-    this.uiChanged_(this.cancelValue_);
   }
 
   /** @param {!Event} event */
   onRemove_(event) {
-    this.imagePreviewSrc = '';
-    this.errorMsg = '';
-    window.localStorage.removeItem(this.preference);
-    this.uiChanged_('');
+    this.imagePreviewSrc_ = '';
+    this.errorMsg_ = '';
+    window.localStorage.removeItem(PREFERENCE);
   }
 
   /** @param {!Event} event */
@@ -213,18 +126,16 @@ export class TerminalSettingsBackgroundImageElement extends
     });
     reader.readAsDataURL(file);
     event.target.value = null;
-    this.closeDialog();
   }
 
   /** @param {string} dataUrl */
   onFileLoad_(dataUrl) {
     try {
-      window.localStorage.setItem(this.preference, dataUrl);
-      this.imagePreviewSrc = dataUrl;
-      this.uiChanged_('');
+      window.localStorage.setItem(PREFERENCE, dataUrl);
+      this.imagePreviewSrc_ = dataUrl;
     } catch (e) {
       console.error(e);
-      this.errorMsg = hterm.messageManager.get(
+      this.errorMsg_ = hterm.messageManager.get(
           'TERMINAL_SETTINGS_BACKGROUND_IMAGE_ERROR_SIZE');
     }
   }
--- terminal/js/terminal_settings_background_image_tests.js
+++ terminal/js/terminal_settings_background_image_tests.js
@@ -6,149 +6,76 @@
  * @fileoverview Terminal Settings Background Image Element unit tests.
  */
 
-import {BACKGROUND_IMAGE_CONVERTER, TerminalSettingsBackgroundImageElement}
+import {TerminalSettingsBackgroundImageElement}
     from './terminal_settings_background_image.js';
 
 describe('terminal_settings_background_image_tests.js', () => {
-  const hubble = 'https://www.google.com/earth/images/hubble_crab_neb-lg.jpg';
   const svgDataUrl = 'data:image/svg+xml;base64,' +
       'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciLz4=';
 
   beforeEach(function() {
-    window.preferenceManager =
-      new lib.PreferenceManager(new lib.Storage.Memory());
-    window.preferenceManager.definePreference('background-image', '');
-    window.preferenceManager.definePreference('background-color', '#000');
     window.localStorage.removeItem('background-image');
 
-    this.el = /** @type {!TerminalSettingsBackgroundImageElement} */ (
-        document.createElement('terminal-settings-background-image'));
-    document.body.appendChild(this.el);
+    this.el = null;
+    this.createElement = async function() {
+      if (this.el) {
+        document.body.removeChild(this.el);
+      }
+      this.el = /** @type {!TerminalSettingsBackgroundImageElement} */ (
+          document.createElement('terminal-settings-background-image'));
+      document.body.appendChild(this.el);
 
-    // The element renders asynchronously.
-    return this.el.updateComplete;
+      // The element renders asynchronously.
+      return this.el.updateComplete;
+    };
   });
 
   afterEach(function() {
-    document.body.removeChild(this.el);
-
-    delete window.preferenceManager;
+    if (this.el) {
+      document.body.removeChild(this.el);
+    }
   });
 
-  it('converts-background-image', async function() {
-    const p2d = BACKGROUND_IMAGE_CONVERTER.preferenceToDisplay;
-    assert.equal(p2d(null), '');
-    assert.equal(p2d(''), '');
-    assert.equal(p2d(' '), '');
-    assert.equal(p2d('url(foo)'), 'foo');
-    assert.equal(p2d('url("foo")'), 'foo');
-    assert.equal(p2d("url('foo')"), 'foo');
-
-    const d2p = BACKGROUND_IMAGE_CONVERTER.displayToPreference;
-    assert.equal(d2p(''), '');
-    assert.equal(d2p(' '), '');
-    assert.equal(d2p('foo'), 'url(http://foo)');
-    assert.equal(d2p('http://foo'), 'url(http://foo)');
-    assert.equal(d2p('https://foo'), 'url(https://foo)');
-  });
-
-  it('updates-ui-when-preference-changes', async function() {
-    assert.equal(window.preferenceManager.get('background-image'), '');
-    assert.equal('', this.el.imagePreviewSrc);
-
-    await window.preferenceManager.set('background-image', `url(${hubble})`);
-    assert.equal(this.el.imagePreviewSrc, hubble);
-  });
-
-  it('shows-preview-local-storage-if-exists-else-pref', async function() {
-    await window.preferenceManager.set('background-image', `url(${hubble})`);
-    assert.equal(this.el.imagePreviewSrc, hubble);
-    await window.preferenceManager.set('background-image', '');
-    assert.equal(this.el.imagePreviewSrc, '');
+  it('shows-preview-local-storage-if-exists', async function() {
+    await this.createElement();
+    assert.equal(this.el.imagePreviewSrc_, '');
 
     window.localStorage.setItem('background-image', svgDataUrl);
-    await window.preferenceManager.set('background-image', `url(${hubble})`);
-    assert.equal(this.el.imagePreviewSrc, svgDataUrl);
+    await this.createElement();
+    assert.equal(this.el.imagePreviewSrc_, svgDataUrl);
   });
 
-  it('clears-pref-and-storage-and-preview-on-remove-click', async function() {
+  it('clears-storage-and-preview-on-remove-click', async function() {
     window.localStorage.setItem('background-image', svgDataUrl);
-    await window.preferenceManager.set('background-image', `url(${hubble})`);
+    await this.createElement();
 
     this.el.shadowRoot.querySelector('#bg-remove').click();
 
     await this.el.updateComplete;
     assert.isNull(window.localStorage.getItem('background-image'));
-    assert.equal('', window.preferenceManager.get('background-image'));
-    assert.equal('', this.el.imagePreviewSrc);
+    assert.equal('', this.el.imagePreviewSrc_);
   });
 
   it('shows-correct-elements', async function() {
     // No image is set.
+    await this.createElement();
     assert.isNotNull(this.el.shadowRoot.querySelector('#bg-select'));
     assert.isNull(this.el.shadowRoot.querySelector('img'));
     assert.isNull(this.el.shadowRoot.querySelector('#bg-remove'));
 
     // Image from local storage.
     window.localStorage.setItem('background-image', svgDataUrl);
-    this.el.preferenceChanged_('');
-    await this.el.updateComplete;
-
-    assert.isNull(this.el.shadowRoot.querySelector('#bg-select'));
-    assert.isNotNull(this.el.shadowRoot.querySelector('img'));
-    assert.isNotNull(this.el.shadowRoot.querySelector('#bg-remove'));
-    window.localStorage.removeItem('background-image');
-
-    // Image from prefs.
-    await window.preferenceManager.set('background-image', `url(${hubble})`);
-    await this.el.updateComplete;
+    await this.createElement();
 
     assert.isNull(this.el.shadowRoot.querySelector('#bg-select'));
     assert.isNotNull(this.el.shadowRoot.querySelector('img'));
     assert.isNotNull(this.el.shadowRoot.querySelector('#bg-remove'));
   });
 
-  it('restores-pref-on-cancel', async function() {
-    await window.preferenceManager.set('background-image', `url(${hubble})`);
-    assert.equal(
-        `url(${hubble})`, window.preferenceManager.get('background-image'));
-    await this.el.updateComplete;
-
-    this.el.openDialog();
-    const prefChanged1 = test.listenForPrefChange(
-        window.preferenceManager, 'background-image');
-    const textfield =
-        this.el.shadowRoot.querySelector('terminal-settings-textfield')
-            .shadowRoot.querySelector('terminal-textfield');
-    textfield.value = 'foo';
-    textfield.dispatchEvent(new Event('change'));
-    await prefChanged1;
-    assert.equal('url(http://foo)', window.preferenceManager.get('background-image'));
-
-    const prefChanged2 = test.listenForPrefChange(
-        window.preferenceManager, 'background-image');
-    this.el.shadowRoot.querySelector('terminal-settings-button.cancel').click();
-    await prefChanged2;
-    assert.equal(
-        `url(${hubble})`, window.preferenceManager.get('background-image'));
-  });
-
-  it('clears-pref-and-updates-preview-when-local-selected', async function() {
-    await window.preferenceManager.set('background-image', `url(${hubble})`);
-    assert.equal(
-        `url(${hubble})`, window.preferenceManager.get('background-image'));
-    await this.el.updateComplete;
-
+  it('updates-preview-and-local-storage-when-file-selected', async function() {
+    await this.createElement();
     this.el.onFileLoad_(svgDataUrl);
-    assert.equal('', window.preferenceManager.get('background-image'));
-    assert.equal(svgDataUrl, this.el.imagePreviewSrc);
-  });
-
-  it('clears-local-and-updates-preview-when-pref-set', async function() {
-    window.localStorage.setItem('background-image', svgDataUrl);
-    this.el.value = `url(${hubble})`;
-    this.el.onOk_();
-    assert.isNull(window.localStorage.getItem('background-image'));
-    assert.equal(hubble, this.el.imagePreviewSrc);
+    assert.equal(svgDataUrl, window.localStorage.getItem('background-image'));
+    assert.equal(svgDataUrl, this.el.imagePreviewSrc_);
   });
 });
