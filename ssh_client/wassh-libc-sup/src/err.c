9f698138c4890a237b954fb17a4d0ef80a94f240
Mike Frysinger <vapier@chromium.org>
2022-03-12 02:37:53 -0500

    ssh_client: wassh-libc-sup: add err APIs
    These are useful, trivial to implement, and wasi-sdk already includes
    err.h.  So implement these so packages don't have to define their own
    set of fallbacks.
    
    Change-Id: I86d7b16d7ccedb03a3e8c3f156a72bbe10430f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/3535840
    Reviewed-by: Emil Mikulic <easy@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

---
+++ ssh_client/wassh-libc-sup/src/err.c
@@ -0,0 +1,72 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Implementation for BSD err APIs.
+
+#include <err.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// No header defines this unfortunately.
+extern const char* __progname;
+
+void warn(const char* format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  vwarn(format, args);
+  va_end(args);
+}
+
+void warnx(const char* format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  vwarnx(format, args);
+  va_end(args);
+}
+
+void err(int status, const char* format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  verr(status, format, args);
+  va_end(args);
+}
+
+void errx(int status, const char* format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  verrx(status, format, args);
+  va_end(args);
+}
+
+void vwarn(const char* format, va_list args) {
+  fprintf(stderr, "%s: ", __progname);
+  if (format) {
+    vfprintf(stderr, format, args);
+    fputs(": ", stderr);
+  }
+  perror(NULL);
+}
+
+void vwarnx(const char* format, va_list args) {
+  fprintf(stderr, "%s: ", __progname);
+  if (format) {
+    vfprintf(stderr, format, args);
+  }
+  fprintf(stderr, "\n");
+}
+
+void verr(int status, const char* format, va_list args) {
+  vwarn(format, args);
+  exit(status);
+}
+
+void verrx(int status, const char* format, va_list args) {
+  vwarnx(format, args);
+  exit(status);
+}
