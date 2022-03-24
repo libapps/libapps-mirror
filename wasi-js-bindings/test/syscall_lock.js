e62bacf25d4ecafa5e4857c5d495df99e2b796b2
Emil Mikulic <easy@google.com>
2022-03-24 18:00:34 +1100

    wasi-js-bindings: test de/serialization of TypedArrays.
    Change-Id: I53618b24c91a8b1ee3d5cbe8b174c1c2c802db52
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/3542734
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- wasi-js-bindings/test/syscall_lock.js
+++ wasi-js-bindings/test/syscall_lock.js
@@ -13,13 +13,27 @@ describe('syscall_lock.js', () => {
 /**
  * Check BigInt is correctly serialized and deserialized.
  */
-it('setData and getData', function() {
+it('setData and getData BigInt', () => {
   const buf = new SharedArrayBuffer(64 * 1024);
   const lock = new SyscallLock(buf);
   const data = {events: [{fd_read: {nwritten: BigInt(100)}}]};
   lock.setData(data);
   const deserialized = lock.getData();
   assert.deepStrictEqual(deserialized, data);
+});
+
+/**
+ * Check TypedArray is correctly serialized and deserialized as an Array.
+ */
+it('setData and getData TypedArray', () => {
+  const typedArray = new Uint8Array([1, 2, 3, 4, 5]);
+  const buf = new SharedArrayBuffer(64 * 1024);
+  const lock = new SyscallLock(buf);
+  const data = {foo: [{bar: typedArray}]};
+  const expected = {foo: [{bar: Array.from(typedArray)}]};
+  lock.setData(data);
+  const deserialized = lock.getData();
+  assert.deepStrictEqual(deserialized, expected);
 });
 
 });
