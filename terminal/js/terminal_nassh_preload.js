f68fcacb166ab3778ec5ea36f92a88f44ec8df98
Joel Hockey <joelhockey@chromium.org>
2022-06-02 17:35:49 -0700

    terminal: polyfill runtime.getBackgroundPage() used by gsc
    Latest version of google-smart-card is calling
    runtime.getBackgroundPage()
    
    Change-Id: I0b229bda87b8e7a7e14cfa1a62d42328ee31a2c8
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/3686790
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Emil Mikulic <easy@google.com>

--- terminal/js/terminal_nassh_preload.js
+++ terminal/js/terminal_nassh_preload.js
@@ -4,13 +4,21 @@
  * @suppress {lintChecks}
  */
 
-// Polyfill `getManifest()` so that nassh is happy.
-if (chrome?.runtime && !chrome.runtime.getManifest) {
-  chrome.runtime.getManifest = () => {
-    return /** @type {!chrome.runtime.Manifest} */ ({
-      'name': 'SSH',
-      'version': lib.f.getChromeMilestone(),
-      'icons': {'192': '/images/dev/crostini-192.png'},
-    });
-  };
+// Polyfill `getBackgroundPage()` and `getManifest()` so that nassh is happy.
+if (chrome?.runtime) {
+  if (!chrome.runtime.getBackgroundPage) {
+    chrome.runtime.getBackgroundPage = (callback) => {
+      setTimeout(callback, 0, window);
+    };
+  }
+
+  if (!chrome.runtime.getManifest) {
+    chrome.runtime.getManifest = () => {
+      return /** @type {!chrome.runtime.Manifest} */ ({
+        'name': 'SSH',
+        'version': lib.f.getChromeMilestone(),
+        'icons': {'192': '/images/dev/crostini-192.png'},
+      });
+    };
+  }
 }
