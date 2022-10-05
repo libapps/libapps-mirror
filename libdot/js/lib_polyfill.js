8e9f1fe5f3cef19db8be355ebb7846282766cf47
Mike Frysinger <vapier@chromium.org>
2022-10-05 16:02:52 +0545

    libdot: polyfill: drop Blob fallbacks
    Major browsers released support for these back in 2019/2020,
    so we shouldn't need to polyfill these anymore.
    
    We keep the structure in place so we can polyfill in the future.
    
    Change-Id: Ie20c86cdb93fb31f25e7982b28989eed0a512f85
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/3935691
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>

--- libdot/js/lib_polyfill.js
+++ libdot/js/lib_polyfill.js
@@ -5,45 +5,9 @@
 'use strict';
 
 /**
- * @fileoverview Polyfills for ES2019+ features we want to use.
+ * @fileoverview Polyfills for ES2020+ features we want to use.
  * @suppress {duplicate} This file redefines many functions.
  */
 
 /** @const */
 lib.polyfill = {};
-
-/**
- * https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
- *
- * @return {!Promise<!ArrayBuffer>}
- */
-lib.polyfill.BlobArrayBuffer = function() {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result);
-    reader.onabort = reader.onerror = () => reject(reader);
-    reader.readAsArrayBuffer(this);
-  });
-};
-
-if (typeof Blob.prototype.arrayBuffer != 'function') {
-  Blob.prototype.arrayBuffer = lib.polyfill.BlobArrayBuffer;
-}
-
-/**
- * https://developer.mozilla.org/en-US/docs/Web/API/Blob/text
- *
- * @return {!Promise<string>}
- */
-lib.polyfill.BlobText = function() {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result);
-    reader.onabort = reader.onerror = () => reject(reader);
-    reader.readAsText(this);
-  });
-};
-
-if (typeof Blob.prototype.arrayBuffer != 'function') {
-  Blob.prototype.text = lib.polyfill.BlobText;
-}
