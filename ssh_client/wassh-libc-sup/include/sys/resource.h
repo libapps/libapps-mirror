f121c32243ef670675187e373c6ea86682671973
Mike Frysinger <vapier@chromium.org>
2024-08-09 13:48:48 -0400

    ssh_client: wassh-libc-sup: stub out rlimit APIs
    Mosh sets core limits which we don't support in general,  and really
    none of the resource limits make sense for us, or can't be changed, so
    stub it all our.
    
    Bug: b/40831504
    Change-Id: I5e9f169efc8ce202e5fec676869a22fbe16a63d3
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/5775797
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

---
+++ ssh_client/wassh-libc-sup/include/sys/resource.h
@@ -0,0 +1,28 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/sys_resource.h.html
+
+#ifndef WASSH_SYS_RESOURCE_H
+#define WASSH_SYS_RESOURCE_H
+
+#include_next <sys/resource.h>
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+typedef unsigned long long rlim_t;
+
+struct rlimit {
+  rlim_t rlim_cur;
+  rlim_t rlim_max;
+};
+
+#define getrlimit(resource, rlim) 0
+#define setrlimit(resource, rlim) 0
+
+__END_DECLS
+
+#endif
