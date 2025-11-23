f4634da7e18397ad70cf36455953a5dfa3ebbc29
Mike Frysinger <vapier@chromium.org>
2025-11-23 02:27:06 -0500

    nassh: sshagent stream: rewrite as ES6 class
    This is a straight port with no (intended) logic change.
    
    Change-Id: I7078112203ed7ec6f146d3c767d8d4bea5fcc4bd
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/7201025
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- nassh/js/nassh_stream_sshagent.js
+++ nassh/js/nassh_stream_sshagent.js
@@ -15,86 +15,84 @@ import {Stream} from './nassh_stream.js';
 
 /**
  * Relay ssh-agent messages to an Agent instance.
- *
- * @param {{authAgent: !Agent}} args
- * @constructor
- * @extends {Stream}
  */
-export function SshAgentStream(args) {
-  Stream.call(this);
+export class SshAgentStream extends Stream {
+  /**
+   * @param {{authAgent: !Agent}} options
+   */
+  constructor({authAgent}) {
+    super();
 
-  this.authAgent_ = args.authAgent;
-  this.pendingMessageSize_ = null;
-  this.writeBuffer_ = new Uint8Array(0);
-}
-
-SshAgentStream.prototype = Object.create(Stream.prototype);
-/** @override */
-SshAgentStream.constructor = SshAgentStream;
-
-/**
- * Open a connection to the agent and let it initialize its backends.
- *
- * @param {!Object} settings
- * @override
- */
-SshAgentStream.prototype.open = async function(settings) {
-  try {
-    return this.authAgent_.ping();
-  } catch (e) {
-    console.log(e);
-    throw e;
+    this.authAgent_ = authAgent;
+    this.pendingMessageSize_ = null;
+    this.writeBuffer_ = new Uint8Array(0);
   }
-};
 
-/**
- * Check whether there is enough data in the write buffer to constitute a
- * packet. If so, send packet to Agent and relay its reply.
- *
- * @private
- */
-SshAgentStream.prototype.trySendPacket_ = function() {
-  // See if we've scanned the message length yet (first 4 bytes).
-  if (this.pendingMessageSize_ === null) {
-    if (this.writeBuffer_.length < 4) {
+  /**
+   * Open a connection to the agent and let it initialize its backends.
+   *
+   * @param {!Object} settings
+   * @override
+   */
+  async open(settings) {
+    try {
+      return this.authAgent_.ping();
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+  /**
+   * Check whether there is enough data in the write buffer to constitute a
+   * packet. If so, send packet to Agent and relay its reply.
+   *
+   * @private
+   */
+  trySendPacket_() {
+    // See if we've scanned the message length yet (first 4 bytes).
+    if (this.pendingMessageSize_ === null) {
+      if (this.writeBuffer_.length < 4) {
+        return;
+      }
+
+      // Read the 32-bit message length.
+      const dv = new DataView(
+          this.writeBuffer_.buffer, this.writeBuffer_.byteOffset);
+      this.pendingMessageSize_ = dv.getUint32(0);
+    }
+
+    // See if we've got the message body yet.
+    if (this.writeBuffer_.length < 4 + this.pendingMessageSize_) {
       return;
     }
 
-    // Read the 32-bit message length.
-    const dv = new DataView(
-        this.writeBuffer_.buffer, this.writeBuffer_.byteOffset);
-    this.pendingMessageSize_ = dv.getUint32(0);
+    // Consume header + body.
+    const reqData = this.writeBuffer_.subarray(0, 4 + this.pendingMessageSize_);
+    this.writeBuffer_ = this.writeBuffer_.subarray(
+        4 + this.pendingMessageSize_);
+    // Restart the message process.
+    this.pendingMessageSize_ = null;
+
+    this.authAgent_.handleRequest(reqData)
+        .then(
+            (response) => this.onDataAvailable(
+                btoa(lib.codec.codeUnitArrayToString(response.rawMessage()))));
   }
 
-  // See if we've got the message body yet.
-  if (this.writeBuffer_.length < 4 + this.pendingMessageSize_) {
-    return;
+  /**
+   * Append data to write buffer.
+   *
+   * @param {!ArrayBuffer} data The bytes to append to the current stream.
+   * @override
+   */
+  async write(data) {
+    if (!data.byteLength) {
+      return;
+    }
+
+    this.writeBuffer_ = concatTyped(this.writeBuffer_, new Uint8Array(data));
+
+    setTimeout(this.trySendPacket_.bind(this), 0);
   }
-
-  // Consume header + body.
-  const reqData = this.writeBuffer_.subarray(0, 4 + this.pendingMessageSize_);
-  this.writeBuffer_ = this.writeBuffer_.subarray(4 + this.pendingMessageSize_);
-  // Restart the message process.
-  this.pendingMessageSize_ = null;
-
-  this.authAgent_.handleRequest(reqData)
-      .then(
-          (response) => this.onDataAvailable(
-              btoa(lib.codec.codeUnitArrayToString(response.rawMessage()))));
-};
-
-/**
- * Append data to write buffer.
- *
- * @param {!ArrayBuffer} data The bytes to append to the current stream.
- * @override
- */
-SshAgentStream.prototype.write = async function(data) {
-  if (!data.byteLength) {
-    return;
-  }
-
-  this.writeBuffer_ = concatTyped(this.writeBuffer_, new Uint8Array(data));
-
-  setTimeout(this.trySendPacket_.bind(this), 0);
-};
+}
