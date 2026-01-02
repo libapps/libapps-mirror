5f441ada2c76aba66ed66274213d3a814738bc66
Mike Frysinger <vapier@chromium.org>
2026-01-02 11:41:08 -0500

    nassh: agent: rewrite as ES6 class
    Now that all users of this have switched to ES6, we can migrate the
    base class over now too.
    
    Change-Id: I19779e3bbccbeb97eac5babc4b4bd410285d67bf
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/7456054
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>

--- nassh/js/nassh_agent_backend.js
+++ nassh/js/nassh_agent_backend.js
@@ -12,18 +12,84 @@ import {Identity} from './nassh_agent_message_types.js';
 
 /**
  * Base class for SSH agent backends compatible with Agent.
- *
- * @param {!UserIO} userIO Used for user-facing terminal IO.
- * @constructor
  */
-export function Backend(userIO) {
+export class Backend {
   /**
-   * Reference to object with terminal IO functions.
-   *
-   * @private {!UserIO}
-   * @const
+   * @param {!UserIO} userIO Used for user-facing terminal IO.
    */
-  this.userIO_ = userIO;
+  constructor(userIO) {
+    /**
+     * Reference to object with terminal IO functions.
+     *
+     * @private {!UserIO}
+     * @const
+     */
+    this.userIO_ = userIO;
+  }
+
+  /**
+   * Called once when the client connects to the agent.
+   * The backend should check whether it is fully operational and run
+   * initializations that can fail.
+   *
+   * @return {!Promise<void>} A resolving promise if the
+   *     backend initialized successfully; a rejecting promise otherwise.
+   */
+  ping() {
+    return Promise.resolve();
+  }
+
+  /**
+   * Called when the client sends an AGENTC_REQUEST_IDENTITIES request.
+   *
+   * @see https://tools.ietf.org/id/draft-miller-ssh-agent-00.html#rfc.section.4.4
+   * @return {!Promise<!Array<!Identity>>} A promise resolving to an array of
+   *     SSH identities; a rejecting promise with an error message if the
+   *     request could not be handled.
+   */
+  requestIdentities() {
+    return Promise.reject(Backend.ERR_NOT_IMPLEMENTED);
+  }
+
+  /**
+   * Called when the client sends an AGENTC_SIGN_REQUEST request.
+   *
+   * @see https://tools.ietf.org/id/draft-miller-ssh-agent-00.html#rfc.section.4.5
+   * @param {!Uint8Array} keyBlob The key blob of the key requested to perform
+   *     the signature.
+   * @param {!Uint8Array} data The challenge data to be signed.
+   * @param {number} flags A uint32 treated as a bitfield of signature flags.
+   * @return {!Promise<!Uint8Array>|!Promise<!Error>} A promise resolving to
+   *     the computed signature; a rejecting promise with an error message if
+   *     the request could not be handled.
+   */
+  signRequest(keyBlob, data, flags) {
+    return Promise.reject(Backend.ERR_NOT_IMPLEMENTED);
+  }
+
+  /**
+   * Show a message in the terminal window.
+   *
+   * @param {string} message The message to be shown. Note: The message should
+   *     consist of a localized string obtained via localize.
+   */
+  showMessage(message) {
+    this.userIO_.showMessage(this.BACKEND_ID, message);
+  }
+
+  /**
+   * Show a message in the terminal window and prompt the user for a string.
+   *
+   * @param {string} promptMessage The message that should precede the prompt.
+   *     Note: The message should consist of a localized string obtained via
+   *     localize.
+   * @return {!Promise<string>|!Promise<void>} A promise resolving to the input
+   *     if the user confirms it by pressing enter; a rejecting promise if the
+   *     user cancels the prompt by pressing ESC.
+   */
+  async promptUser(promptMessage) {
+    return this.userIO_.promptUser(this.BACKEND_ID, promptMessage);
+  }
 }
 
 /**
@@ -40,67 +106,3 @@ Backend.prototype.BACKEND_ID = 'stub';
  * @const {!Error}
  */
 Backend.ERR_NOT_IMPLEMENTED = new Error('not implemented');
-
-/**
- * Called once when the client connects to the agent.
- * The backend should check whether it is fully operational and run
- * initializations that can fail.
- *
- * @return {!Promise<void>} A resolving promise if the
- *     backend initialized successfully; a rejecting promise otherwise.
- */
-Backend.prototype.ping = function() {
-  return Promise.resolve();
-};
-
-/**
- * Called when the client sends an AGENTC_REQUEST_IDENTITIES request.
- *
- * @see https://tools.ietf.org/id/draft-miller-ssh-agent-00.html#rfc.section.4.4
- * @return {!Promise<!Array<!Identity>>} A promise resolving to an array of SSH
- *     identities; a rejecting promise with an error message if the request
- *     could not be handled.
- */
-Backend.prototype.requestIdentities = function() {
-  return Promise.reject(Backend.ERR_NOT_IMPLEMENTED);
-};
-
-/**
- * Called when the client sends an AGENTC_SIGN_REQUEST request.
- *
- * @see https://tools.ietf.org/id/draft-miller-ssh-agent-00.html#rfc.section.4.5
- * @param {!Uint8Array} keyBlob The key blob of the key requested to perform
- *     the signature.
- * @param {!Uint8Array} data The challenge data to be signed.
- * @param {number} flags A uint32 treated as a bitfield of signature flags.
- * @return {!Promise<!Uint8Array>|!Promise<!Error>} A promise resolving to
- *     the computed signature; a rejecting promise with an error message if the
- *     request could not be handled.
- */
-Backend.prototype.signRequest = function(keyBlob, data, flags) {
-  return Promise.reject(Backend.ERR_NOT_IMPLEMENTED);
-};
-
-/**
- * Show a message in the terminal window.
- *
- * @param {string} message The message to be shown. Note: The message should
- *     consist of a localized string obtained via localize.
- */
-Backend.prototype.showMessage = function(message) {
-  this.userIO_.showMessage(this.BACKEND_ID, message);
-};
-
-/**
- * Show a message in the terminal window and prompt the user for a string.
- *
- * @param {string} promptMessage The message that should precede the prompt.
- *     Note: The message should consist of a localized string obtained via
- *     localize.
- * @return {!Promise<string>|!Promise<void>} A promise resolving to the input
- *     if the user confirms it by pressing enter; a rejecting promise if the
- *     user cancels the prompt by pressing ESC.
- */
-Backend.prototype.promptUser = async function(promptMessage) {
-  return this.userIO_.promptUser(this.BACKEND_ID, promptMessage);
-};
