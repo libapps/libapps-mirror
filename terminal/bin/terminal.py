4baa886dd128021db35d10649f34c9da50076008
Mike Frysinger <vapier@chromium.org>
2026-01-04 20:47:33 -0500

    terminal: mkdeps: leave symlinks in place
    These tools are not parallel safe -- if you run load_tests and lint at
    the sametime, the links will get removed before the other finishes, and
    it ends up failing in a confusing way.  Or the symlinks don't get fully
    removed.
    
    Depending on how the tools are interrupted, symlinks can get orphaned,
    and if nassh itself removes files, inks stay orphaned.
    
    Change the code to symlink all the files and leave them in place.  If
    a symlink already exists, ignore it.  If a symlink is broken, delete it.
    
    This also helps with testing the code in-situ without having to rebuild
    deps all the time.
    
    Change-Id: I3b5535c9cbd6a8dfb21bafb785dee51e416d83f2
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/7486464
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- terminal/bin/terminal.py
+++ terminal/bin/terminal.py
@@ -23,41 +23,45 @@ sys.path.insert(0, str(LIBAPPS_DIR / "libdot" / "bin"))
 import libdot  # pylint: disable=unused-import
 
 
-class SymlinkNasshFiles:
-    """A context manager to symlink nassh files.
+def nassh_files(inc_rollup: bool = False) -> list[Path]:
+    """Get the list of files we import from nassh."""
+    paths = []
+    for p in (NASSH_DIR / "js").glob("*.js"):
+        if p.name.endswith(".shim.js"):
+            # The shim file is an input to rollup.  Add outputs.
+            if inc_rollup:
+                paths.append(p.parent / f"{p.name[:-8]}.rollup.js")
+        elif p.name.endswith(".rollup.js"):
+            # Already handled via the .shim.js case.
+            pass
+        elif "_test" not in p.name:
+            paths.append(p)
+    return sorted(x.relative_to(NASSH_DIR) for x in paths)
 
-    All the symlinked files will be deleted when the context manager exits.
-    """
 
-    def __init__(self):
-        self.filenames = []
-
-    def __enter__(self):
-        nassh_js_dir = NASSH_DIR / "js"
-        paths = {p for p in nassh_js_dir.glob("*.js") if "test" not in p.name}
-        # Manually adding them since they might not exist until mkdeps() is run.
-        paths.update(
-            nassh_js_dir / f"deps_{x}.rollup.js"
-            for x in ("indexeddb-fs", "lit", "xterm")
-        )
-
-        logging.info("symlinking nassh files")
-        for path in paths:
-            try:
-                (JS_DIR / path.name).symlink_to(path)
-            except FileExistsError:
-                logging.warning(
+def _symlink_nassh_files() -> None:
+    """Symlink nassh files for testing & developing locally."""
+    logging.info("Symlinking nassh files")
+    for path in nassh_files(inc_rollup=True):
+        dest = JS_DIR / path.name
+        try:
+            dest.symlink_to(NASSH_DIR / path)
+        except FileExistsError:
+            if dest.exists() and not dest.is_symlink():
+                logging.error(
                     "file %s already exists in terminal/js", path.name
                 )
-            else:
-                self.filenames.append(path.name)
+                raise
 
-    def __exit__(self, *args):
-        logging.info("removing symlinked files")
-        for filename in self.filenames:
-            (JS_DIR / filename).unlink()
+    # Clear stale links.
+    for path in JS_DIR.glob("*.js"):
+        if not path.exists():
+            logging.info("Removing stale symlink: %s", path)
+            path.unlink()
 
 
 def mkdeps(_opt):
     """Build the required deps for the testing/linting."""
     libdot.run([NASSH_DIR / "bin/mkdeps"])
+    # Must come after nassh mkdeps.
+    _symlink_nassh_files()
