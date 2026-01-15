7dfe354214e47ea77d22ab04c6e49dee01f3318c
Mike Frysinger <vapier@chromium.org>
2026-01-14 22:27:00 -0500

    libdot: storage: chrome: pull quota write retry into a helper function
    This currently only protects calls to setItem which misses out on other
    write operations like setItems or removeItems.  Refactor the logic so it
    could be reused in more places, although don't migrate that just yet as
    we need to rewrite the callback logic first.
    
    Bug: b/40874862
    Change-Id: If13d5a6c785f9e85bbc3524db20a0bb9b350f156
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/7475466
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- libdot/js/lib_storage_chrome.js
+++ libdot/js/lib_storage_chrome.js
@@ -5,6 +5,33 @@
 import {lib} from './lib.js';
 
 /**
+ * Helper to retry operations when exceeding write quota.
+ *
+ * @param {function(): !Promise<void>} callback The operation to (re)try.
+ * @param {number=} delay How long (in msec) to sleep after quota error.
+ */
+async function retryQuotaErrors(callback, delay = 1000) {
+  const checkError = async () => {
+    const err = lib.f.lastError();
+    if (err) {
+      // Doesn't seem to be any better way of handling this.
+      // https://crbug.com/764759
+      if (err.indexOf('MAX_WRITE_OPERATIONS')) {
+          console.warn(`Will retry write after exceeding quota:`, err);
+          return false;
+      } else {
+        console.error(`Unknown runtime error: ${err}`);
+      }
+    }
+    return true;
+  };
+
+  while (!await callback().then(checkError)) {
+    await new Promise((resolve) => setTimeout(resolve, delay));
+  }
+}
+
+/**
  * chrome.storage based class with an async interface that is interchangeable
  * with other lib.Storage.* implementations.
  */
@@ -58,27 +85,7 @@ lib.Storage.Chrome = class extends lib.Storage {
    * @override
    */
   async setItem(key, value) {
-    return new Promise((resolve) => {
-      const onComplete = () => {
-        const err = lib.f.lastError();
-        if (err) {
-          // Doesn't seem to be any better way of handling this.
-          // https://crbug.com/764759
-          if (err.indexOf('MAX_WRITE_OPERATIONS')) {
-            console.warn(`Will retry '${key}' save after exceeding quota:`,
-                         err);
-            setTimeout(() => this.setItem(key, value).then(onComplete), 1000);
-            return;
-          } else {
-            console.error(`Unknown runtime error: ${err}`);
-          }
-        }
-
-        resolve();
-      };
-
-      this.setItems({[key]: value}).then(onComplete);
-    });
+    return retryQuotaErrors(() => this.setItems({[key]: value}));
   }
 
   /**
