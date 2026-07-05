76eeeb3ee94c8ac1eb1f891183a59b74ac4cd3da
Mike Frysinger <vapier@chromium.org>
2026-07-05 17:53:07 +0545

    ssh_client: wassh-libc-sup: drop socket() argument validation
    All the argument checking done in here is also performed in the syscall
    handler by necessity -- we need to decode the domain/type to figure out
    what kind of socket to create, and reject unknown values.  So having the
    same logic in the C code gains us nothing.  We don't need to optimize for
    invalid values, and if we want to add/extend the APIs, we have to update
    both places.  So drop all the C checks and leave it to the JS runtime.
    
    This is also required to support flags in the type field since the C code
    rejects all such values.
    
    Change-Id: I3b02e2ca3744e97291473749230ec4d76d4a8959
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/8042064
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>

--- ssh_client/wassh-libc-sup/src/socket.c
+++ ssh_client/wassh-libc-sup/src/socket.c
@@ -13,58 +13,18 @@
 #include "bh-syscalls.h"
 #include "debug.h"
 
-int socket(int domain, int c_type, int protocol) {
-  _ENTER("domain=%i type=%i protocol=%i", domain, c_type, protocol);
+int socket(int domain, int type, int protocol) {
+  _ENTER("domain=%i type=%i protocol=%i", domain, type, protocol);
 
   // Make sure preopens have processed before we create a socket as that will
   // update the file descriptor table, and preopen logic will fail when it hits
   // a non-preopen fd.
   __wasilibc_populate_preopens();
 
-  // We don't support much here currently.
-  // 0: The default for most things.
-  // -1: Our fake delayed hostname logic from getaddrinfo.
-  if (protocol != 0 && protocol != -1 &&
-      !(c_type == SOCK_STREAM && protocol == IPPROTO_TCP) &&
-      !(c_type == SOCK_DGRAM && protocol == IPPROTO_UDP)) {
-    _EXIT("|protocol| unknown");
-    errno = EINVAL;
-    return -1;
-  }
+  // Don't check arguments as the syscall handler will do it.
+  // We don't need to optimize for bad/unknown values.
 
-  // Only support UNIX sockets (ssh-agent), IPv4, and IPv6.
-  switch (domain) {
-    case AF_UNIX:
-    case AF_INET:
-    case AF_INET6:
-      break;
-    default:
-      _EXIT("|domain| unknown");
-      errno = EINVAL;
-      return -1;
-  }
-
-  // Maybe add these if anyone wants them.
-  if (c_type & (SOCK_NONBLOCK | SOCK_CLOEXEC)) {
-    _EXIT("|type| flags unsupported");
-    errno = EINVAL;
-    return -1;
-  }
-
-  // Only support TCP & UDP protocols.
-  int sys_type;
-  switch (c_type) {
-    case SOCK_DGRAM:
-    case SOCK_STREAM:
-      sys_type = c_type;
-      break;
-    default:
-      _EXIT("|type| unknown");
-      errno = EINVAL;
-      return -1;
-  }
-
-  int ret = sock_create(domain, sys_type, protocol);
+  int ret = sock_create(domain, type, protocol);
   _EXIT("ret = %i", ret);
   return ret;
 }
